f9c25bec7e14edab4fae1ec669aa93d27a5143d9
EstebanGomez1 <79277954+EstebanGomez1@users.noreply.github.com>
2025-08-30 22:04:46 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ class PTv3_deteccion(nn.Module):
     def __init__(self, grid_size: int):
         super(PTv3_deteccion, self).__init__()
         self.grid_size = grid_size
+        self.pool = nn.AdaptiveAvgPool2d((4, 4))  # Por ejemplo: reduce [64, 24, 24] a [64, 4, 4]
+
         self.point_encoder = PointTransformerV3(
             in_channels=1,
             enc_depths=(1, 1, 1, 1, 1),
@@ -22,45 +24,50 @@ class PTv3_deteccion(nn.Module):
         )
 
         self.feature_layer = nn.Sequential(      
-            nn.Linear(64, 64),
+            nn.Linear(32*6*6, 128),
             nn.ReLU(),      
-            nn.Linear(64, 32)
+            nn.Linear(128, 32),
+            nn.ReLU(),      
+            nn.Linear(32, 6)
         )
 
-        # 64*23*23   18432
         self.clf_head = nn.Sequential(
-            nn.Linear(36864, 128),
-            nn.ReLU(),
+            nn.Linear(32*6*6, 128),
+            nn.ReLU(),      
             nn.Linear(128, 32),
-            nn.ReLU(),
+            nn.ReLU(),      
             nn.Linear(32, 8)
         ) 
         self.reg_head = nn.Sequential(
-            nn.Linear(36864, 128),
-            nn.ReLU(),
+            nn.Linear(32*6*6, 128),
+            nn.ReLU(),      
             nn.Linear(128, 32),
-            nn.ReLU(),
+            nn.ReLU(),      
             nn.Linear(32, 6)
         )  
         self.cyc_head_sin = nn.Sequential(
-            nn.Linear(36864, 64),
-            nn.ReLU(),
-            nn.Linear(64, 1),
+            nn.Linear(32*6*6, 128),
+            nn.ReLU(),      
+            nn.Linear(128, 32),
+            nn.ReLU(),      
+            nn.Linear(32, 1),
             nn.Tanh()
         )
         self.cyc_head_cos = nn.Sequential(
-            nn.Linear(36864, 64),
-            nn.ReLU(),
-            nn.Linear(64, 1),
+            nn.Linear(32*6*6, 128),
+            nn.ReLU(),      
+            nn.Linear(128, 32),
+            nn.ReLU(),      
+            nn.Linear(32, 1),
             nn.Tanh()
         )
 
         self.conv_block = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU()
-            #nn.AdaptiveAvgPool2d((1,1))
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((6,6))
         )
 
 
@@ -78,8 +85,6 @@ class PTv3_deteccion(nn.Module):
             "grid_size": self.grid_size,
         }
         point_features = self.point_encoder(points_dict)
-        #feats_mean = torch.mean(point_features["feat"], dim=0, keepdim=True)  # [1, D]
-        #embedding = self.feature_layer(feats_mean)
         #_-------_
         grid_feat = self.scatter_grid_pooling(
             point_features["coord"].detach(),
@@ -88,58 +93,18 @@ class PTv3_deteccion(nn.Module):
             grid_size= 24, #6/0.25
             device=device
         )
-        grid_feat = grid_feat.unsqueeze(0)  # [1, F]
+        grid_feat = grid_feat.unsqueeze(0) 
 
-        #############
-        """# Visualizar y guardar
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"heatmap_{timestamp}.png"
-        grid_np = grid_feat[0, 0].detach().cpu().numpy()
-
-        plt.figure(figsize=(6, 6))
-        plt.imshow(grid_np, cmap='hot', origin='lower')
-        plt.colorbar(label='Número de puntos')
-        plt.title("Distribución de puntos en la rejilla")
-
-        # Asegúrate de que la carpeta exista
-        folder = "grids"
-        os.makedirs(folder, exist_ok=True)
-        filepath = os.path.join(folder, filename)
-        plt.savefig(filepath)
-        plt.close()
-
-        # Extraer coordenadas XY
-        points_np = all_points[:, :3].detach().cpu().numpy()
-        x, y = points_np[:, 0], points_np[:, 1]
-
-        # Crear figura 2D
-        plt.figure(figsize=(6, 6))
-        plt.scatter(x, y, c='blue', s=1)  # puedes cambiar color o tamaño de punto
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.title("Nube de puntos proyectada en XY")
-        plt.axis('equal')  # para que los ejes tengan la misma escala
-
-        # Guardar imagen
-        cloud_img_filename = f"cloud_{timestamp}.png"
-        cloud_img_path = os.path.join(folder, cloud_img_filename)
-        plt.savefig(cloud_img_path)
-        plt.close()"""
-        #############
-
-
-
-        #embedding = self.feature_layer(grid_feat)
         embedding = self.conv_block(grid_feat)  
         embedding = embedding.view(1, -1)  
         #_-------_
-        normalized_embedding = embedding
-        logits = self.clf_head(normalized_embedding)     # [batch, 8]
-        reg_out = self.reg_head(normalized_embedding)    # [batch, 6]
-        sin_out = self.cyc_head_sin(normalized_embedding)
-        cos_out = self.cyc_head_cos(normalized_embedding)
+        logits = self.clf_head(embedding)     # [batch, 8]
+        reg_out = self.reg_head(embedding)    # [batch, 6]
+        sin_out = self.cyc_head_sin(embedding)
+        cos_out = self.cyc_head_cos(embedding)
         cyc_out = torch.cat([sin_out, cos_out], dim=1) 
-        #cyc_out = F.normalize(cyc_out, dim=1)
+        
+        #cyc_out = torch.cat([sin_out, cos_out], dim=1) 
         return logits, reg_out, cyc_out
     
     def scatter_grid_pooling(self, coords, feats, res_x=0.25, grid_size=24, device="cpu"):
@@ -149,7 +114,6 @@ class PTv3_deteccion(nn.Module):
         y = coords[:, 1]
 
         F = feats.shape[1]
-        grid = torch.zeros((F, grid_size, grid_size), device=device)
 
         half_extent = (grid_size * res_x) / 2
 
@@ -169,3 +133,20 @@ class PTv3_deteccion(nn.Module):
         grid = grid.view(F, grid_size, grid_size)
 
         return grid
+
+    """global_feat = self.global_statistics_pooling(
+            point_features["coord"],
+            point_features["feat"]
+        )"""
+    def global_statistics_pooling(self, coords, feats):
+        # coords: [N, 3], feats: [N, F]
+        xy = coords[:, :2]                      # [N, 2]
+        feats_orig = feats                     # [N, F]
+        
+        mean_xy = torch.mean(xy, dim=0)        # [2]
+        cov_xy = torch.cov(xy.T).flatten()     # [4]
+        mean_feats = torch.mean(feats_orig, dim=0)  # [F]
+
+        # Concatenar todo en un solo vector
+        global_feat = torch.cat([mean_xy, cov_xy, mean_feats], dim=0)  # [2 + 4 + F]
+        return global_feat  # [D]
